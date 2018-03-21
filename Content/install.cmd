99dba65e0c0981b2558cc89287dec0664ed7ba59
soltm <mariusz.soltys@suffolk.gov.uk>
2018-03-21 16:26:40 +0000

    0.0.3

--- Content/install.cmd
+++ Content/install.cmd
@@ -6,7 +6,8 @@ IF NOT EXIST "%PHP_INI_SCAN_DIR%" (
     mkdir "%PHP_INI_SCAN_DIR%"
 )
 
-cp "imagick.ini" "%PHP_INI_SCAN_DIR%"
-
+IF EXIST "%PHP_INI_SCAN_DIR%" (
+    cp "imagick.ini" "%PHP_INI_SCAN_DIR%"
+)
 
 unzip -qq zip\AzureImageMagick.zip -d d:\home\SiteExtensions\ImageMagickPHP71x86\lib
