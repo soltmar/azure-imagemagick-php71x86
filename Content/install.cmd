9a9ecded345124303e056793ad6701a41bc28e4a
soltm <mariusz.soltys@suffolk.gov.uk>
2018-03-21 14:17:27 +0000

    initial commit

---
+++ Content/install.cmd
@@ -0,0 +1,9 @@
+IF NOT EXIST "%HOME%\site\applicationHost.xdt" (
+    cp "applicationHost.xdt" "%HOME%\site\applicationHost.xdt"
+)
+
+IF EXIST "%PHP_INI_SCAN_DIR%" (
+    cp "imagick.ini" "%PHP_INI_SCAN_DIR%"
+)
+
+unzip -qq lib\AzureImageMagick.zip -d d:\home\site\SiteExtensions\ImageMagickPHP71x86\lib
